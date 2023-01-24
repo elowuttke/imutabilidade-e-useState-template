9e1b4e0036739cda031de43c7d37462845c2df0d
elowuttke <elowuttke@gmail.com>
2023-01-23 21:16:56 -0300

    class exercises done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Garagem } from './Componentes/Garagem'
 import { GlobalStyled } from './GlobalStyled'
+import { useState } from 'react'
 
 export default function App() {
+// Declarar o estado
+//    [estado, função que modifica o valor do estado]
+  const [nome, setNome] = useState("Labenu");
+
+  const mudaNome = () => {
+    setNome("Conway");
+  };
+// Criar um estado na garagem com o  carro, passar os valores para dentro do componente Carro  
+
   return (
     <div className="App">
       <GlobalStyled />
-
-      <Garagem nome="Labenu" />
+      <Garagem nome={nome} mudaNome={mudaNome} />
     </div>
   )
 }
